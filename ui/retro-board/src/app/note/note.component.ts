9dff4355405412727531e374c3c10534b56d55e3
unknown <brodny@wp.pl>
2020-07-02 21:27:23 +0200

    Using note-component to display note

--- ui/retro-board/src/app/note/note.component.ts
+++ ui/retro-board/src/app/note/note.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Note } from '../model/note';
 
 @Component({
   selector: 'app-note',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./note.component.less']
 })
 export class NoteComponent implements OnInit {
+
+  @Input() note: Note;
 
   constructor() { }
 
